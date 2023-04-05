f44760480b61155a7d42ea6388912f726b08de79
Fernando Mendes <mendesfnando@gmail.com>
2023-04-05 17:12:52 -0300

    #2 - arrays syntax

---
+++ 2_ADVANCING_IN_TYPES/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3, 4];
+numbers.push(5);
+console.log(numbers[3]);
+const names = ['Leticia', 'Fernando'];
+let objs = [{ 'test': 'lalala' }];
+console.log(objs);
+// 2 -  another syntax of arrays
+const nums = [1, 2, 3, 4, 5];
+nums.push(300);
+console.log(nums);
---
+++ 2_ADVANCING_IN_TYPES/index.ts
@@ -0,0 +1,16 @@
+// 1 - arrays
+let numbers: number[] = [1, 2, 3, 4]
+
+numbers.push(5)
+console.log(numbers[3])
+
+const names: string[] = ['Leticia', 'Fernando']
+
+let objs: object[] = [{'test': 'lalala'}]
+console.log(objs)
+
+// 2 -  another syntax of arrays
+const nums: Array<number> = [1, 2, 3, 4, 5]
+
+nums.push(300)
+console.log(nums)
